c9203bb24581ffb5b372939b9376eaf30e0825aa
Dam Nguyen <kubombo0007@gmail.com>
2020-09-26 01:05:57 +0700

    Updates

---
+++ precache-manifest.95a0b03b4032e785d118ddba58443669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb48f21de3a0dac3a66aac3a5d324b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19f6fb2749e66dea4091",
+    "url": "/static/css/main.b341497f.chunk.css"
+  },
+  {
+    "revision": "6abdc03196b7362700f2",
+    "url": "/static/js/2.628b232b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.628b232b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f6fb2749e66dea4091",
+    "url": "/static/js/main.547b3ce7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
